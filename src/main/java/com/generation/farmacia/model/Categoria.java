b5cde0cf9c132ec81cd51701b9a36c1cffe22d31
Natan Macedo <natan.mac.n@gmail.com>
2025-05-28 11:54:21 -0300

    :sparkles: Realizado CRUD de Categoria + testes no insomnia

---
+++ src/main/java/com/generation/farmacia/model/Categoria.java
@@ -0,0 +1,37 @@
+package com.generation.farmacia.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+
+@Entity
+@Table(name = "tb_categorias")
+public class Categoria {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotBlank(message = "A categoria não pode ser vazia!")
+	private String categoria;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(String categoria) {
+		this.categoria = categoria;
+	}
+
+}
